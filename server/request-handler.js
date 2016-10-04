d2cb05479077337ca56d84e00fdb415399954de6
tcwood <travis.cwood@gmail.com>
2016-10-04 12:01:12 -0700

    Include underscore lib and refactor/clean request-handler.js

--- server/request-handler.js
+++ server/request-handler.js
@@ -3,6 +3,7 @@ var data = require('./classes/messages');
 var fs = require('fs');
 var path = require('path');
 var url = require('url');
+var _ = require('underscore');
 
 //global vars and defaults
 var defaultCorsHeaders = {
@@ -23,64 +24,59 @@ var requestHandler = function(request, response) {
   //Building complete file path based off of current working directory
   var uri = url.parse(request.url).pathname;
   var filePath = path.join(process.cwd(), 'client/', uri);
-  console.log(filePath);
 
   var contentTypeByExt = {
     '.html': 'text/html',
     '.css': 'text/css',
-    '.js' : 'text/javascript'
+    '.js': 'text/javascript'
   };
-
   var statusCode;
-  if (request.method === 'GET' && request.url === '/classes/messages') {
-    headers['Content-Type'] = 'application/JSON';
-    statusCode = 200;
+
+  
+  if (request.url === '/classes/messages' || request.method === 'OPTIONS') {
+    if (request.method === 'GET') {
+      statusCode = 200;
+    } else if (request.method === 'POST') {
+      statusCode = 201;
+      request.on('data', function(chunk) {
+        //Extend message obj with expected properties
+        chunkObj = _.extend(JSON.parse(chunk), {
+          objectId: idCounter,
+          createdAt: new Date()
+        });
+        data.results.push(chunkObj);
+        idCounter++;
+      });
+    } else {  //If request is OPTIONS...
+      statusCode = 200;
+      headers['Access-Control-Allow-Credentials'] = false;
+    }
+    headers['Content-Type'] = 'application/JSON'; 
     response.writeHead(statusCode, headers);
     response.end(JSON.stringify(data));
     return;
-  } else if (request.method === 'POST' && request.url === '/classes/messages') {
-    headers['Content-Type'] = 'application/JSON';
-    statusCode = 201;
-    request.on('data', function(chunk) {
-      chunkObj = JSON.parse(chunk);
-      chunkObj.objectId = idCounter;
-      idCounter++;
-      chunkObj.createdAt = new Date();
-      data.results.push(chunkObj);
-    });
-    response.writeHead(statusCode, headers);
-    response.end(JSON.stringify(data));
-    return;
-  } else if (request.method === 'OPTIONS') {
-    statusCode = 200;
-    headers['Access-Control-Allow-Credentials'] = false;
-    headers['Content-Type'] = 'text/plain';
-    response.writeHead(statusCode, headers);
-    response.end(JSON.stringify(data));
-    return;
-  } 
-  else {
+
+  } else {
     fs.exists(filePath, function(exists) {
+      //File not found
       if (!exists) {
-        statusCode = 404;
         headers['Content-Type'] = 'text/plain';
-        response.writeHead(statusCode, headers);
+        response.writeHead(404, headers);
         response.end('404: Not found');
         return;
       }
-
-      if(fs.statSync(filePath).isDirectory()) {
+      // If requesting root server page, then load client index.html
+      if (fs.statSync(filePath).isDirectory()) {
         filePath += 'index.html';
       }
 
       fs.readFile(filePath, function(error, content) {
-        if (error) {
-          console.log('ERROR: ' + error);
+        if (error) {  //Internal server error
           headers['Content-Type'] = 'text/plain';
           response.writeHead(500, headers);
           response.end(error + '\n');
           return;
-        } else {
+        } else { //Write html, css, js client files
           var contentType = contentTypeByExt[path.extname(filePath)];
           if (contentType) {
             headers['Content-Type'] = contentType;
@@ -89,11 +85,8 @@ var requestHandler = function(request, response) {
           response.end(content, 'utf-8');
         }
       });
-    })
+    });
   }
-
-  
-   // else response.end(JSON.stringify(data));
 };
 
 exports.requestHandler = requestHandler;
